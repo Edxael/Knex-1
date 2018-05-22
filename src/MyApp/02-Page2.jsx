482db8804a88ff7e9c1728001103322186315b02
Edxael <zerphyr.r@gmail.com>
2018-05-22 14:18:56 -0600

    add bakend

--- src/MyApp/02-Page2.jsx
+++ src/MyApp/02-Page2.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
+import axios from 'axios'
 
 export default class extends React.Component{
     render(){
+
+        const getData = () => {
+            console.log("----------------------------------- \n  GET all the Records:")
+
+            axios.get('http://localhost:8080/zyphex')
+                .then( (response) => { return response.data } )
+                .then( (data) => { console.log(data) })
+                .catch( (error) => { console.log(error) })
+        }
+
         return(
             <div>
                 <h1>Page 2</h1>
+
+                <div>
+                    <h4>GET ALL</h4>
+                    <button onClick={ getData }>GET All Data</button>
+                    <br/><br/>
+                </div>
             </div>
         )
     }
